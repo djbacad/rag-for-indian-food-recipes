9ac92a5d0082f1861bda309664d246b0bdf1fc04
djbacad <djbacad@gmail.com>
2024-09-08 00:41:09 +0800

    Update download script

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -7,8 +7,9 @@ kaggle.api.authenticate()
 # Dataset spec
 dataset_name = 'kanishk307/6000-indian-food-recipes-dataset' 
 dataset_version = 'latest'  # 'latest' to get the most recent version
-download_path = './datasets'
+download_path = '../datasets'
 
 # Download the dataset
 kaggle.api.dataset_download_files(dataset_name, path=download_path, unzip=True)
 print(f"Dataset '{dataset_name}' downloaded successfully to '{download_path}'")
+
