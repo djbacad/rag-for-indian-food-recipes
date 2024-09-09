a3ad8750ca5855b307ba7f70e0ca62f5229170ec
djbacad <djbacad@gmail.com>
2024-09-09 20:03:44 +0800

    Replace model with mixtral8x7b

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import argparse
 from utils.embedder import Embedder
 from utils.chroma_db_manager import ChromaDBManager
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import warnings
 # Suppress all warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -20,23 +20,23 @@ def main(question):
     context = chroma_db_manager.query_embeddings(query_embedding)
 
     # Initialize the tokenizer and model
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small", device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
     # Prepare input text for the model
     input_text = f"Question: {question}. Context: {context}"
-    input_ids = tokenizer(input_text, return_tensors='pt').input_ids.to("cuda")
+    input_ids = tokenizer(input_text, max_length=512, truncation=True, return_tensors='pt').input_ids.to("cuda")
 
    # Generate a response
     print("Generating Response...⌛")
-    outputs = model.generate(input_ids, max_new_tokens=2000, min_new_tokens=1000)
+    outputs = model.generate(input_ids, max_new_tokens=2000, min_new_tokens=100)
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     print(generated_text)
-    # # Display the generated response
-    # print("Generated Answer:")
-    # print(generated_response[0]['generated_text'])
-    # print("Done ✅")
+    # Display the generated response
+    print("Generated Answer:")
+    print(generated_response[0]['generated_text'])
+    print("Done ✅")
 
 if __name__ == "__main__":
     # Set up argument parsing
